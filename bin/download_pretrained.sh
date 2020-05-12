fcea8be18573dd375f1b7c8353c9022241edd9a2
alexander.rakowski <alexander.rakowski@hpi.de>
2020-05-12 17:43:05 +0200

    download_pretrained.sh

--- bin/download_pretrained.sh
+++ bin/download_pretrained.sh
@@ -1,6 +1,6 @@
 ROOT_DIR=$1
 
-for FILE_ID in {0..4}; do
+for FILE_ID in {0..10799}; do
   EXTRACTED_PATH=${ROOT_DIR}/${FILE_ID}
   ZIP_PATH=${EXTRACTED_PATH}.zip
   wget https://storage.googleapis.com/disentanglement_lib/unsupervised_study_v1/${FILE_ID}.zip -O ${ZIP_PATH}
