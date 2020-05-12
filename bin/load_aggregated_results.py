5eac3bfe83037ed6e63147f66154cb1182d774ff
alexander.rakowski <alexander.rakowski@hpi.de>
2020-05-12 20:37:32 +0200

    load_aggregated_results.py

--- bin/load_aggregated_results.py
+++ bin/load_aggregated_results.py
@@ -10,28 +10,35 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
+METRICS = (
+    'evaluation_results.informativeness_test',
+    'evaluation_results.disentanglement',
+    'evaluation_results.completeness',
+    'evaluation_results.discrete_mig',
+    # 'evaluation_results.eval_accuracy',
+    'beta_vae_sklearn',
+    'factor_vae_metric',
+    'evaluation_results.modularity_score',
+    'evaluation_results.explicitness_score_test',
+    # 'evaluation_results.num_active_dims',
+    'evaluation_results.SAP_score',
+    'evaluation_results.gaussian_total_correlation',
+    # 'evaluation_results.gaussian_wasserstein_correlation',
+    'evaluation_results.gaussian_wasserstein_correlation_norm',
+    'evaluation_results.mutual_info_score',
+)
 
-# TODO all metrics in one plot
+
 def plot_results(results_file):
     df = pd.read_json(results_file)
 
-    for metric in (
-            'evaluation_results.informativeness_test',
-            'evaluation_results.disentanglement',
-            'evaluation_results.completeness',
-            'evaluation_results.discrete_mig',
-            # 'evaluation_results.eval_accuracy',
-            'beta_vae_sklearn',
-            'factor_vae_metric',
-            'evaluation_results.modularity_score',
-            'evaluation_results.explicitness_score_test',
-            # 'evaluation_results.num_active_dims',
-            'evaluation_results.SAP_score',
-            'evaluation_results.gaussian_total_correlation',
-            'evaluation_results.gaussian_wasserstein_correlation',
-            'evaluation_results.gaussian_wasserstein_correlation_norm',
-            'evaluation_results.mutual_info_score',
-    ):
+    # TODO nicer way of mapping reg. strengths
+    df[MODEL_COL_STR] = df[MODEL_COL_STR] + '_' + df['train_config.dim_wise_l1_vae.lmbd_l1'].map(str)
+    df[MODEL_COL_STR] = df[MODEL_COL_STR].str.replace('dim_wise_l1_', '')
+    df[MODEL_COL_STR] = df[MODEL_COL_STR].str.replace('_nan', '')
+
+    fig, axes = plt.subplots(nrows=3, ncols=4, figsize=(30, 30))
+    for metric, ax in zip(METRICS, axes.flatten()):
         print()
         if metric in ('beta_vae_sklearn', 'factor_vae_metric'):
             metric_df = df.loc[df['evaluation_config.evaluation.name'].str.contains(metric)]
@@ -47,8 +54,10 @@ def plot_results(results_file):
             y=metric,
             data=metric_df,
             cut=0,
+            ax=ax,
         )
-        plt.show()
+
+    plt.show()
 
 
 if __name__ == '__main__':
