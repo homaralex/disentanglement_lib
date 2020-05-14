e3128362319aeba18b86640d5890823c3d17ed72
alexander.rakowski <alexander.rakowski@hpi.de>
2020-05-14 16:04:39 +0200

    masked dense layer

--- disentanglement_lib/methods/shared/layers.py
+++ disentanglement_lib/methods/shared/layers.py
@@ -2,19 +2,25 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.layers.core import Dense
 from tensorflow.python.ops import init_ops
+from tensorflow.python.eager import context
+from tensorflow.python.framework import common_shapes
+from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import gen_math_ops
+from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn
+from tensorflow.python.ops import standard_ops
 
 
 class _BaseMaskedLayer:
-    def __init__(self, perc_sparse=0., *args, **kwargs):
+    def __init__(self, perc_sparse=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.perc_sparse = perc_sparse
 
     @property
     def mask_shape(self):
-        raise NotImplementedError()
+        return self.kernel.shape[-2:]
 
     def _init_mask(self):
         mask_val = (np.random.random(self.mask_shape) >= self.perc_sparse).astype('float')
@@ -34,12 +40,12 @@ class _BaseMaskedLayer:
 
 
 class MaskedConv2d(_BaseMaskedLayer, tf.layers.Conv2D):
-    @property
-    def mask_shape(self):
-        return self.kernel.shape[-2:]
-
     def call(self, inputs):
-        outputs = self._convolution_op(inputs, self.kernel * self.mask)
+        outputs = self._convolution_op(
+            inputs,
+            # that's the actual change
+            self.kernel * self.mask
+        )
 
         if self.use_bias:
             if self.data_format == 'channels_first':
@@ -77,7 +83,7 @@ def masked_conv2d(
         trainable=True,
         name=None,
         reuse=None,
-        perc_sparse=0.,
+        perc_sparse=0,
 ):
     layer = MaskedConv2d(
         filters=filters,
@@ -104,5 +110,71 @@ def masked_conv2d(
     return layer.apply(inputs)
 
 
-class MaskedDense(Dense):
-    pass
+class MaskedDense(_BaseMaskedLayer, Dense):
+    def call(self, inputs):
+        inputs = ops.convert_to_tensor(inputs)
+        rank = common_shapes.rank(inputs)
+        if rank > 2:
+            # Broadcasting is required for the inputs.
+            outputs = standard_ops.tensordot(
+                inputs,
+                # that's the actual change
+                self.kernel * self.mask,
+                [[rank - 1], [0]])
+            # Reshape the output back to the original ndim of the input.
+            if not context.executing_eagerly():
+                shape = inputs.shape.as_list()
+                output_shape = shape[:-1] + [self.units]
+                outputs.set_shape(output_shape)
+        else:
+            # Cast the inputs to self.dtype, which is the variable dtype. We do not
+            # cast if `should_cast_variables` is True, as in that case the variable
+            # will be automatically casted to inputs.dtype.
+            if not self._mixed_precision_policy.should_cast_variables:
+                inputs = math_ops.cast(inputs, self.dtype)
+            outputs = gen_math_ops.mat_mul(
+                inputs,
+                # that's the actual change
+                self.kernel * self.mask
+            )
+        if self.use_bias:
+            outputs = nn.bias_add(outputs, self.bias)
+        if self.activation is not None:
+            return self.activation(outputs)  # pylint: disable=not-callable
+        return outputs
+
+
+def masked_dense(
+        inputs, units,
+        activation=None,
+        use_bias=True,
+        kernel_initializer=None,
+        bias_initializer=init_ops.zeros_initializer(),
+        kernel_regularizer=None,
+        bias_regularizer=None,
+        activity_regularizer=None,
+        kernel_constraint=None,
+        bias_constraint=None,
+        trainable=True,
+        name=None,
+        reuse=None,
+        perc_sparse=0,
+):
+    layer = MaskedDense(
+        units=units,
+        activation=activation,
+        use_bias=use_bias,
+        kernel_initializer=kernel_initializer,
+        bias_initializer=bias_initializer,
+        kernel_regularizer=kernel_regularizer,
+        bias_regularizer=bias_regularizer,
+        activity_regularizer=activity_regularizer,
+        kernel_constraint=kernel_constraint,
+        bias_constraint=bias_constraint,
+        trainable=trainable,
+        name=name,
+        _scope=name,
+        _reuse=reuse,
+        perc_sparse=perc_sparse,
+    )
+    return layer.apply(inputs)
--- disentanglement_lib/methods/shared/layers_test.py
+++ disentanglement_lib/methods/shared/layers_test.py
@@ -2,10 +2,82 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.ops import init_ops
 
-from disentanglement_lib.methods.shared.layers import masked_conv2d
+from disentanglement_lib.methods.shared.layers import masked_conv2d, masked_dense
+
+_CHECKPOINT_PATH = "/tmp/model.ckpt"
+
+
+def test_dense():
+    tf.reset_default_graph()
+
+    def get_ops():
+        input_tensor = tf.placeholder(tf.float32, input_shape, name='input')
+        _non_sparse_output_tensor = masked_dense(
+            inputs=input_tensor,
+            units=10,
+            bias_initializer=init_ops.zeros_initializer(),
+            kernel_initializer=init_ops.ones_initializer(),
+        )
+        _sparse_output_tensor = masked_dense(
+            perc_sparse=.5,
+            inputs=input_tensor,
+            units=10,
+            bias_initializer=init_ops.zeros_initializer(),
+            kernel_initializer=init_ops.ones_initializer(),
+        )
+
+        return _non_sparse_output_tensor, _sparse_output_tensor
+
+    input_shape = (1, 20)
+    non_sparse_output_tensor, sparse_output_tensor = get_ops()
+
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        sess.run(init)
+
+        non_sparse_output = (sess.run(
+            non_sparse_output_tensor, feed_dict={
+                'input:0': np.ones(input_shape),
+            }))
+        assert not (non_sparse_output != 20).any()
+
+        sparse_output = (sess.run(
+            sparse_output_tensor, feed_dict={
+                'input:0': np.ones(input_shape),
+            }))
+        # note that there is a non-zero probability that this will fail anyway because masks are generated randomly -
+        # try re-running the test then
+        assert (sparse_output != 20).any()
+
+        # check if the masking is consistent for future calls
+        sparse_output2 = (sess.run(
+            sparse_output_tensor, feed_dict={
+                'input:0': np.ones(input_shape),
+            }))
+        assert np.array_equal(sparse_output, sparse_output2)
+
+        saver.save(sess, _CHECKPOINT_PATH)
+
+    tf.reset_default_graph()
+    non_sparse_output_tensor, sparse_output_tensor = get_ops()
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        saver.restore(sess, _CHECKPOINT_PATH)
+
+        # check if the masking is consistent for loaded graphs
+        sparse_output2 = (sess.run(
+            sparse_output_tensor, feed_dict={
+                'input:0': np.ones(input_shape),
+            }))
+        assert np.array_equal(sparse_output, sparse_output2)
 
 
 def test_conv2d():
+    tf.reset_default_graph()
+
     def get_ops():
         input_tensor = tf.placeholder(tf.float32, input_shape, name='input')
         _non_sparse_output_tensor = masked_conv2d(
@@ -45,6 +117,8 @@ def test_conv2d():
             sparse_output_tensor, feed_dict={
                 'input:0': np.ones(input_shape),
             }))
+        # note that there is a non-zero probability that this will fail anyway because masks are generated randomly -
+        # try re-running the test then
         assert (sparse_output != 20 * 3 * 3).any()
         # check if whole channels are masked (as opposed to masking on a filter-level)
         assert not (sparse_output % 3 * 3 != 0).any()
@@ -56,14 +130,14 @@ def test_conv2d():
             }))
         assert np.array_equal(sparse_output, sparse_output2)
 
-        saver.save(sess, "/tmp/model.ckpt")
+        saver.save(sess, _CHECKPOINT_PATH)
 
     tf.reset_default_graph()
     non_sparse_output_tensor, sparse_output_tensor = get_ops()
     saver = tf.train.Saver()
 
     with tf.Session() as sess:
-        saver.restore(sess, "/tmp/model.ckpt")
+        saver.restore(sess, _CHECKPOINT_PATH)
 
         # check if the masking is consistent for loaded graphs
         sparse_output2 = (sess.run(
@@ -75,4 +149,5 @@ def test_conv2d():
 
 # TODO run as tf.TestCase
 if __name__ == '__main__':
+    test_dense()
     test_conv2d()
