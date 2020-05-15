36cd85ac741df16ba42436069937e5db77d58771
alexander.rakowski <alexander.rakowski@hpi.de>
2020-05-15 14:43:26 +0200

    update load_aggregated_results.py

--- aggregated_results/extract_beta_vae_results.py
+++ aggregated_results/extract_beta_vae_results.py
@@ -4,7 +4,7 @@ DLIB_RESULTS_PATH = 'aggregated_results/dlib_results.json'
 
 dlib_df = pd.read_json(DLIB_RESULTS_PATH)
 beta_vae_df = dlib_df.loc[
-    (dlib_df['train_config.dataset.name'] == "'dsprites_full'")
-    & (dlib_df['train_config.model.name'] == "'beta_vae'")
+    (dlib_df['train_config.model.name'] == "'beta_vae'")
+    # & (dlib_df['train_config.dataset.name'] == "'dsprites_full'")
     ]
 beta_vae_df.to_json(DLIB_RESULTS_PATH.replace('dlib_results', 'dlib_beta_vae_results'))
--- scripts/load_aggregated_results.py
+++ scripts/load_aggregated_results.py
@@ -1,4 +1,5 @@
 import argparse
+from pathlib import Path
 
 import pandas as pd
 import seaborn as sns
@@ -29,76 +30,100 @@ METRICS = (
     'evaluation_results.mutual_info_score',
 )
 
+PLOT_DIR = Path('plots')
+PLOT_DIR.mkdir(exist_ok=True)
+
 
 def plot_results(results_file):
-    dlib_df = pd.read_json(DLIB_RESULTS_PATH)
-    dlib_df = dlib_df.loc[dlib_df['train_config.vae.beta'] == 1]
-
-    df = pd.read_json(results_file)
-
-    # TODO nicer way of mapping reg. strengths
-    df[MODEL_COL_STR] = df[MODEL_COL_STR] + '_' + df['train_config.dim_wise_l1_vae.lmbd_l1'].map("{:.2e}".format)
-    df[MODEL_COL_STR] = df[MODEL_COL_STR].str.replace('dim_wise_l1_', '')
-    df[MODEL_COL_STR] = df[MODEL_COL_STR].str.replace('_nan', '')
-    df[MODEL_COL_STR] = df[MODEL_COL_STR].str.replace('_vae', '')
-    df = df.loc[df['train_config.dim_wise_l1_vae.lmbd_l1'].between(1e-5, 1e-3)]
-
-    df = pd.concat((df, dlib_df))
-
-    fig_violin, axes_violin = plt.subplots(nrows=3, ncols=4, figsize=(30, 30))
-    fig_box, axes_box = plt.subplots(nrows=3, ncols=4, figsize=(30, 30))
-    fig_mean, axes_mean = plt.subplots(nrows=3, ncols=4, figsize=(30, 30))
-    for metric, ax_violin, ax_box, ax_mean in zip(
-            METRICS,
-            axes_violin.flatten(),
-            axes_box.flatten(),
-            axes_mean.flatten(),
+    for dataset in (
+            'dsprites_full',
+            'cars3d',
     ):
-        print()
-        if metric in ('beta_vae_sklearn', 'factor_vae_metric'):
-            metric_df = df.loc[df['evaluation_config.evaluation.name'].str.contains(metric)]
-            metric = 'evaluation_results.eval_accuracy'
-        else:
-            metric_df = df.loc[df[metric].notna()]
+        for method in (
+                'masked',
+                'dim_wise_l1',
+        ):
+            for beta in (
+                    1,
+                    16,
+            ):
+                out_dir = PLOT_DIR / method / f'beta_{beta}'
+                out_dir.mkdir(exist_ok=True, parents=True)
 
-        print(metric_df.groupby(MODEL_COL_STR)[metric].mean().reset_index().sort_values(metric, ascending=False))
+                df = pd.read_json(results_file)
+                df = df.loc[df['train_config.model.name'].str.contains(method)]
 
-        metric_df = metric_df.sort_values(MODEL_COL_STR)
+                if method == 'dim_wise_l1':
+                    # TODO nicer way of mapping reg. strengths
+                    df[MODEL_COL_STR] = df[MODEL_COL_STR] + '_' + df['train_config.dim_wise_l1_vae.lmbd_l1'].map(
+                        "{:.2e}".format)
+                    df[MODEL_COL_STR] = df[MODEL_COL_STR].str.replace('dim_wise_l1_', '')
+                    df[MODEL_COL_STR] = df[MODEL_COL_STR].str.replace('_vae', '')
+                    df = df.loc[df['train_config.dim_wise_l1_vae.lmbd_l1'].between(1e-5, 1e-3)]
+                elif method == 'masked':
+                    df[MODEL_COL_STR] = df[MODEL_COL_STR] + '_' + df['train_config.conv_encoder.perc_sparse'].map(
+                        lambda x: round(x, 2)
+                    ).map(str)
 
-        sns.violinplot(
-            x=MODEL_COL_STR,
-            y=metric,
-            data=metric_df,
-            cut=0,
-            ax=ax_violin,
-        )
-        for tick in ax_violin.get_xticklabels():
-            tick.set_rotation(45)
+                dlib_df = pd.read_json(DLIB_RESULTS_PATH)
+                df = pd.concat((df, dlib_df))
+                df = df.loc[(df['train_config.vae.beta'] == beta) & (df['train_config.dataset.name'] == f"'{dataset}'")]
 
-        sns.boxplot(
-            x=MODEL_COL_STR,
-            y=metric,
-            data=metric_df,
-            ax=ax_box,
-        )
-        for tick in ax_box.get_xticklabels():
-            tick.set_rotation(45)
+                fig_violin, axes_violin = plt.subplots(nrows=3, ncols=4, figsize=(30, 30))
+                fig_box, axes_box = plt.subplots(nrows=3, ncols=4, figsize=(30, 30))
+                fig_mean, axes_mean = plt.subplots(nrows=3, ncols=4, figsize=(30, 30))
+                for metric, ax_violin, ax_box, ax_mean in zip(
+                        METRICS,
+                        axes_violin.flatten(),
+                        axes_box.flatten(),
+                        axes_mean.flatten(),
+                ):
+                    print()
+                    if metric in ('beta_vae_sklearn', 'factor_vae_metric'):
+                        metric_df = df.loc[df['evaluation_config.evaluation.name'].str.contains(metric)]
+                        metric = 'evaluation_results.eval_accuracy'
+                    else:
+                        metric_df = df.loc[df[metric].notna()]
 
-        # group and aggreagate to obtain means per model
-        metric_df = metric_df.groupby(MODEL_COL_STR)[metric].mean()
-        sns.stripplot(
-            x=metric_df.index,
-            y=metric_df,
-            ax=ax_mean,
-            size=25,
-        )
-        for tick in ax_mean.get_xticklabels():
-            tick.set_rotation(45)
+                    print(
+                        metric_df.groupby(MODEL_COL_STR)[metric].mean().reset_index().sort_values(metric,
+                                                                                                  ascending=False))
 
-    fig_violin.savefig('plots/violin.png')
-    fig_box.savefig('plots/box.png')
-    fig_mean.savefig('plots/mean.png')
-    plt.show()
+                    metric_df = metric_df.sort_values(MODEL_COL_STR)
+
+                    sns.violinplot(
+                        x=MODEL_COL_STR,
+                        y=metric,
+                        data=metric_df,
+                        cut=0,
+                        ax=ax_violin,
+                    )
+                    for tick in ax_violin.get_xticklabels():
+                        tick.set_rotation(45)
+
+                    sns.boxplot(
+                        x=MODEL_COL_STR,
+                        y=metric,
+                        data=metric_df,
+                        ax=ax_box,
+                    )
+                    for tick in ax_box.get_xticklabels():
+                        tick.set_rotation(45)
+
+                    # group and aggreagate to obtain means per model
+                    metric_df = metric_df.groupby(MODEL_COL_STR)[metric].mean()
+                    sns.stripplot(
+                        x=metric_df.index,
+                        y=metric_df,
+                        ax=ax_mean,
+                        size=25,
+                    )
+                    for tick in ax_mean.get_xticklabels():
+                        tick.set_rotation(45)
+
+                fig_violin.savefig(out_dir / f'{dataset}_violin.png')
+                fig_box.savefig(out_dir / f'{dataset}_box.png')
+                fig_mean.savefig(out_dir / f'{dataset}_mean.png')
 
 
 if __name__ == '__main__':
