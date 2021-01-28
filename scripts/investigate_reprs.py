5484f5c1afd6339e571ad8634ee1e04ae541240d
alexander.rakowski <alexander.rakowski@hpi.de>
2021-01-28 18:28:52 +0100

    WIP: investigate_reprs script - encodings

--- scripts/investigate_reprs.py
+++ scripts/investigate_reprs.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from collections import defaultdict
 
 import numpy as np
 import gin.tf
@@ -7,7 +8,6 @@ import tensorflow as tf
 import tensorflow_hub as hub
 from tensorflow import gfile
 
-from disentanglement_lib.visualize.visualize_model import sigmoid, tanh
 from disentanglement_lib.data.ground_truth import named_data
 from disentanglement_lib.utils import results
 
@@ -15,7 +15,7 @@ from disentanglement_lib.utils import results
 def main(
         model_dir,
         # output_dir,
-        num_pics,
+        num_points,
         overwrite=True,
 ):
     # Fix the random seed for reproducibility.
@@ -40,15 +40,49 @@ def main(
     module_path = os.path.join(model_dir, 'tfhub')
 
     with hub.eval_function_for_module(module_path) as f:
-        real_pics = dataset.sample_observations(num_pics, random_state)
+        sampled_latents = dataset.state_space.sample_latent_factors(
+            num=num_points,
+            random_state=random_state,
+        )
+        sampled_points = dataset.sample_all_factors(
+            latent_factors=sampled_latents,
+            random_state=random_state,
+        )
 
-        result = batched_encoder(f, images=real_pics)
+        # encode 'real' images
+        real_pics = dataset.sample_observations_from_all_factors(factors=sampled_points, random_state=random_state)
 
-        means = result['mean']
-        logvars = result['logvar']
+        encodings = batched_encoder(f, images=real_pics)
+
+        means = encodings['mean']
+        logvars = encodings['logvar']
 
         print(means.std(axis=0).round(3))
         print(np.exp(logvars).mean(axis=0).round(3))
+
+        # investigate factor-encoding relations
+        per_factor_encodings = defaultdict(list)
+        for factor_idx, factor_size in enumerate(dataset.all_factor_sizes):
+            xs = np.arange(0, factor_size, 1)
+            if factor_size < 2:
+                continue
+
+            for curr_point in sampled_points:
+                curr_point = np.expand_dims(curr_point.copy(), axis=0)
+                points = np.repeat(curr_point, factor_size, axis=0)
+                points[:, factor_idx] = xs
+
+                ys = dataset.sample_observations_from_all_factors(factors=points, random_state=random_state)
+                zs = batched_encoder(f, images=ys)['mean']
+
+                per_factor_encodings[factor_idx].append(zs)
+
+        per_factor_stds = np.zeros((len(dataset.all_factor_sizes), means.shape[1]))
+        for factor_idx, factor_encodings in per_factor_encodings.items():
+            per_factor_stds[factor_idx] = np.array(factor_encodings).std(axis=1).mean(axis=0)
+
+        for factor_idx, factor_std in enumerate(per_factor_stds):
+            print(factor_idx, factor_std.round(3))
 
 
 def batched_encoder(
@@ -74,11 +108,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_dir', type=str)
     parser.add_argument('--output_dir', type=str)
-    parser.add_argument('--num_pics', type=int, default=64)
+    parser.add_argument('--num_points', type=int, default=64)
     args = parser.parse_args()
 
     main(
         model_dir=args.model_dir,
         # output_dir=args.output_dir,
-        num_pics=args.num_pics,
+        num_points=args.num_points,
     )
